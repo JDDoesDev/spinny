5c776d6d61294930536e917418c6fab7fab02be2
JD Flynn <jflynn8@gmail.com>
2024-06-15 10:22:58 -0500

    Adding gitignore and changing color

--- card.gd
+++ card.gd
@@ -5,10 +5,10 @@ var selected = false
 func _selected():
 	selected = true
 	modulate = Color.html("005500ff")
-	
+
 func _unselected():
 	selected = false
-	modulate = Color.html("ffffffff") 
+	modulate = Color.html("000000ff")
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event.is_action("grab"):
